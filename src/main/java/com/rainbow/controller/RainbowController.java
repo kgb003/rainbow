dc1f3b98c1087b5646da4449d220f347c53aa48f
권기범 <kibeom.kwon@partner.sk.com>
2017-01-13 13:56:21 +0900

    셋팅

--- src/main/java/com/rainbow/controller/RainbowController.java
+++ src/main/java/com/rainbow/controller/RainbowController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class bootController {
+public class RainbowController {
 	
 	@GetMapping("/")
 	public String main(Model model){
