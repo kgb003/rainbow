1796413116f1dfc3ef6568353309783ebd1fbcc2
권기범 <kibeom.kwon@partner.sk.com>
2017-01-13 13:33:08 +0900

    프로젝트 생성

---
+++ src/main/java/com/rainbow/controller/bootController.java
@@ -0,0 +1,15 @@
+package com.rainbow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class bootController {
+	
+	@GetMapping("/")
+	public String main(Model model){
+		
+		return "index";
+	}
+}
